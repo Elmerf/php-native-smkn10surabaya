08125c1a6df12424a52e6d39f3e015caaf28433f
elmerfiqi <fiqi.elmer@syngenta.com>
2023-10-15 17:14:30 +0700

    Improve Edit and Delete Materi

--- crudmateri.php
+++ crudmateri.php
@@ -3,32 +3,53 @@ include 'koneksi.php';
 
 //TOMBOL SIMPAN
 if (isset($_POST['btn_simpanmateri'])) {
+  if (isset($_POST['hal']) == "edit") {
+    // HAPUS DATA TERLEBIH DAHULU
+    $result = mysqli_query($koneksi, "SELECT namafile FROM filepdf WHERE id_file = '$_POST[id]'");
+    $data = mysqli_fetch_array($result);
 
-  $target_dir = "filepdf/";
-  $target_file = $target_dir . basename($_FILES["namafile"]["name"]);
-  //DATA DIEDIT
-  if (isset($_GET['hal']) == "edit") {
-    $edit = mysqli_query($koneksi, "UPDATE filepdf SET namafile = '$_POST[namafile]', jenismateri = '$_POST[jenismateri]' WHERE id_file = '$_GET[id]'");
+    if ($data) {
+      $target_dir = "filepdf/";
+      $namafile = $data['namafile'];
+      $target_file = $target_dir . basename($namafile);
 
-    //UJI JIKA EDIT DATA SUKSES
-    if ($edit) {
-      echo "<script>alert('File berhasil diubah'); document.location='materiguru.php'</script>";
-    } else {
-      echo "<script>alert('File tidak berhasil diubah'); document.location='materiguru.php'</script>";
+      if (file_exists($target_file)) {
+        unlink($target_file);
+      }
+
+      $hapus = mysqli_query($koneksi, "UPDATE filepdf set namafile = '' WHERE id_file = '$_POST[id]'");
+
+      // JIKA HAPUS BERHASIL BARU EDIT
+      if ($hapus) {
+        $filename = $_FILES["namafile"]["name"];
+        $target_file = $target_dir . basename($_FILES["namafile"]["name"]);
+        move_uploaded_file($_FILES["namafile"]["tmp_name"], $target_file);
+
+        $edit = mysqli_query($koneksi, "UPDATE filepdf SET namafile = '$filename', jenismateri = '$_POST[jenismateri]' WHERE id_file = '$_POST[id]'");
+
+        //UJI JIKA EDIT DATA SUKSES
+        if ($edit) {
+          echo "<script>alert('File berhasil diubah'); document.location='materiguru.php'</script>";
+        } else {
+          echo "<script>alert('File tidak berhasil diubah'); document.location='materiguru.php'</script>";
+        }
+      }
     }
   } else {
+    $target_dir = "filepdf/";
+    $target_file = $target_dir . basename($_FILES["namafile"]["name"]);
     //JIKA BUKAN EDIT DATA AKAN DISIMPAN BARU
     $filename = $_FILES["namafile"]["name"];
     move_uploaded_file($_FILES["namafile"]["tmp_name"], $target_file);
 
     $tambah = mysqli_query($koneksi, "INSERT INTO filepdf (namafile, jenismateri) VALUE ('$filename','$_POST[jenismateri]')");
-  }
 
-  //UJI JIKA SIMPAN DATA SUKSES
-  if ($tambah) {
-    echo "<script>alert('File berhasil ditambahkan'); document.location='materiguru.php'</script>";
-  } else {
-    echo "<script>alert('File tidak berhasil ditambahkan'); document.location='materiguru.php'</script>";
+    //UJI JIKA SIMPAN DATA SUKSES
+    if ($tambah) {
+      echo "<script>alert('File berhasil ditambahkan'); document.location='materiguru.php'</script>";
+    } else {
+      echo "<script>alert('File tidak berhasil ditambahkan'); document.location='materiguru.php'</script>";
+    }
   }
 }
 
@@ -38,26 +59,27 @@ $varjenismateri = "";
 
 //TOMBOL EDIT ATAU HAPUS DIKLIK
 if (isset($_GET['hal'])) {
-  //TOMBOL EDIT
-  if ($_GET['hal'] == "edit") {
-    //TAMPILKAN DATA
-    $tampil = mysqli_query($koneksi, "SELECT * FROM filepdf WHERE id_file='$_GET[id]'");
-    $data = mysqli_fetch_array($tampil);
+  if ($_GET['hal'] == "hapus") {
+    //HAPUS DATA
+    $result = mysqli_query($koneksi, "SELECT namafile FROM filepdf WHERE id_file = '$_GET[id]'");
+    $data = mysqli_fetch_array($result);
 
     if ($data) {
-      //JIKA DATA DITEMUKAN, MAKA DATA DITAMPUNG VARIABEL
-      $varnamafile = $data['namafile'];
-      $varjenismateri = $data['jenismateri'];
-    }
-  } else if ($_GET['hal'] == "hapus") {
-    //HAPUS DATA
-    $hapus = mysqli_query($koneksi, "DELETE FROM filepdf WHERE id_file = '$_GET[id]'");
+      $target_dir = "filepdf/";
+      $namafile = $data['namafile'];
+      $target_file = $target_dir . basename($namafile);
 
-    //UJI JIKA HAPUS DATA SUKSES
-    if ($hapus) {
-      echo "<script>alert('File berhasil dihapus'); document.location='materiguru.php'</script>";
-    } else {
-      echo "<script>alert('File tidak berhasil dihapus'); document.location='materiguru.php'</script>";
+      if (file_exists($target_file)) {
+        unlink($target_file);
+      }
+
+      $hapus = mysqli_query($koneksi, "DELETE FROM filepdf WHERE id_file = '$_GET[id]'");
+      //UJI JIKA HAPUS DATA SUKSES
+      if ($hapus) {
+        echo "<script>alert('File berhasil dihapus'); document.location='materiguru.php'</script>";
+      } else {
+        echo "<script>alert('File tidak berhasil dihapus'); document.location='materiguru.php'</script>";
+      }
     }
   }
 }
